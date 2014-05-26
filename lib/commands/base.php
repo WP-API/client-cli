d4d9bf53c8e89d4aaff2ead514b9dba13b93ccb3
Ryan McCue <me@ryanmccue.info>
2014-05-26 16:45:58 +1000

    Use new locator/authenticator for finding connection

--- lib/commands/base.php
+++ lib/commands/base.php
@@ -2,18 +2,32 @@
 
 namespace WP_JSON\CLI\Commands;
 
+use Exception;
+use WPAPI;
 use WP_CLI;
 use WP_CLI_Command;
+use WP_JSON\CLI\Authenticator;
+use WP_JSON\CLI\Locator;
 
 class Base extends WP_CLI_Command {
 
 	/**
 	 * Get a connection object for making requests.
 	 *
-	 * @return \WPAPI
+	 * @return WPAPI
 	 */
-	protected function get_connection( $assoc_args ) {
-		return new \WPAPI( $assoc_args['url'], $assoc_args['username'], $assoc_args['password'] );
+	protected function get_connection( $url ) {
+		$locator = new Locator();
+		$url = $locator->locate( $url );
+
+		$auth = Authenticator::get_for_site( $url );
+		if ( empty( $auth ) ) {
+			throw new Exception( 'No authentication available; use `wp api oauth1 connect` first' );
+		}
+
+		$api = new WPAPI( $url );
+		$api->setAuth( $auth );
+		return $api;
 	}
 
 	/**
