09238d16295aab15a65caa673b73aa2c15ca36c5
Ryan McCue <me@ryanmccue.info>
2014-05-26 16:47:55 +1000

    Add `wp api user get-current` command

--- lib/commands/user.php
+++ lib/commands/user.php
@@ -53,4 +53,49 @@ class User extends Base {
 
 	}
 
+	/**
+	 * ## OPTIONS
+	 *
+	 * <url>
+	 * : URL for the WordPress site
+	 *
+	 * [--format=<format>]
+	 * : Output format ('table', 'json', 'csv', 'ids', 'count')
+	 *
+	 * @subcommand get-current
+	 * @when before_wp_load
+	 */
+	public function get_current( $args, $assoc_args ) {
+		try {
+			$api = $this->get_connection();
+			$response = $api->get( '/users/me' );
+			$response->throw_for_status();
+
+			$data = json_decode( $response->body, true );
+
+			if ( empty( $assoc_args['format'] ) ) {
+				$assoc_args['format'] = 'table';
+			}
+
+			$fields = array( 'ID', 'username', 'name', 'email', 'roles' );
+
+			if ( $assoc_args['format'] !== 'json' ) {
+				$data = \WP_CLI\Utils\iterator_map( array( $data ), function( $user ) {
+					unset( $user['meta'] );
+					$user['roles'] = implode( ',', $user['roles'] );
+
+					if ( ! empty( $user['capabilities'] ) ) {
+						$user['capabilities'] = array_filter( $user['capabilities'] );
+						$user['capabilities'] = implode( ',', $user['capabilities'] );
+					}
+					return $user;
+				} );
+			}
+
+			\WP_CLI\Utils\format_items( $assoc_args['format'], $data, $fields );
+		}
+		catch ( Exception $e ) {
+			WP_CLI::error( $e->getMessage() );
+		}
+	}
 }
